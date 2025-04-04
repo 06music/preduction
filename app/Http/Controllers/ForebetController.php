a0d86d9d930eaf820a922940138f2295d14316cc
Bouhlel Mahmoud <50195772+06music@users.noreply.github.com>
2025-04-04 16:11:35 +0100

    Updassste ForebetController.php

--- app/Http/Controllers/ForebetController.php
+++ app/Http/Controllers/ForebetController.php
@@ -7,47 +7,52 @@ use Illuminate\Support\Facades\DB;
 
 class ForebetController extends Controller
 {
-    $matches = DB::table('forebet_matches as fm')
-        ->select(
-            'id',
-            'timestamp',
-            'game',
-            'time_str',
-            'iso_time',
-            'score',
-            'half_time_score',
-            'et',
-            'et_minute',
-            'prediction',
-            'prob_1',
-            'prob_x',
-            'prob_2',
-            'home_team',
-            'away_team',
-            'home_rank',
-            'away_rank',
-            'match_url',
-            'live_odds',
-            'home_pts',
-            'home_gp',
-            'home_w',
-            'home_d',
-            'home_l',
-            'home_gf',
-            'home_ga',
-            'home_gd',
-            'away_pts',
-            'away_gp',
-            'away_w',
-            'away_d',
-            'away_l',
-            'away_gf',
-            'away_ga',
-            'away_gd'
-        )
-        ->orderByDesc('timestamp')
-        ->get();
+   public function getMatches()
+    {
+        $matches = DB::table('forebet_matches as fm')
+            ->select(
+                'id',
+                'timestamp',
+                'game',
+                'time_str',
+                'iso_time',
+                'score',
+                'half_time_score',
+                'et',
+                'et_minute',
+                'prediction',
+                'prob_1',
+                'prob_x',
+                'prob_2',
+                'home_team',
+                'away_team',
+                'home_rank',
+                'away_rank',
+                'match_url',
+                'live_odds',
+                'home_pts',
+                'home_gp',
+                'home_w',
+                'home_d',
+                'home_l',
+                'home_gf',
+                'home_ga',
+                'home_gd',
+                'away_pts',
+                'away_gp',
+                'away_w',
+                'away_d',
+                'away_l',
+                'away_gf',
+                'away_ga',
+                'away_gd'
+            )
+            ->orderByDesc('timestamp')
+            ->get();
 
-    return response()->json($matches);
-   }
+        return response()->json($matches);
+    }
+
+
 }
+
